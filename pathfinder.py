a42d9fbefe5b8fb2a6a1c60dfa8c7dfab6538a9f
Josef Kolář <thejoeejoee@gmail.com>
2016-04-09 23:04:52 +0200

    pathfinder rewrote into two classes

--- pathfinder.py
+++ pathfinder.py
@@ -3,31 +3,21 @@ from bisect import insort
 from requests import post, get
 
 
-EMPTY = 0
-TREASURE = 1
-BOT = 2
-BLOCK = 3
-LASER_BATTERY_BOT = 4
+class FieldType():
+    EMPTY = 0
+    TREASURE = 1
+    BOT = 2
+    BLOCK = 3
+    LASER_BATTERY_BOT = 4
+
 
 NORTH = 0
 EAST = 1
 SOUTH = 2
 WEST = 3
 
-MAP_HEIGHT = 80
-MAP_WIDTH = 80
-
-post('http://localhost:44822/admin',
-     dict(
-         map_height=MAP_HEIGHT,
-         map_width=MAP_WIDTH,
-         bots=1,
-         treasures=1,
-         blocks=10,
-         maze_game=True,
-         battery_game=True,
-         laser_game=True
-     ))
+MAP_HEIGHT = 50
+MAP_WIDTH = 50
 
 
 class Map(tuple):
@@ -35,6 +25,134 @@ class Map(tuple):
         assert hasattr(position, '__iter__') and len(position) == 2
         assert all(map(lambda x: x >= 0, position))
         return super().__getitem__(position[0])[position[1]]
+
+
+class MapRating():
+    def __init__(self, game: dict, game_map: Map):
+        self._game = game
+        self._game_map = game_map
+        self._batteries_enabled = game.get('game_info', {}).get('battery_game', False)
+        self._lasers_enabled = game.get('game_info', {}).get('laser_game', False)
+        self._start_bot_position = next(iter(
+            get_field_occurrences(
+                FieldType.LASER_BATTERY_BOT if self._batteries_enabled or self._lasers_enabled else FieldType.BOT,
+                self._game_map,
+                your_bot=True)
+        ))
+        self._rated_map = None
+
+    def get_rated_map(self):
+        if not self._rated_map:
+            self._rated_map = self.rate_game_map(
+                self._game_map,
+                self._start_bot_position,
+                self._game_map[self._start_bot_position].get('orientation')
+            )
+        return self._rated_map
+
+    def rate_game_map(self, game_map: Map, start_position: tuple, last_direction: int):
+        """Rate game map by price of actions:
+            step - 1 + 1 if batteries are enabled
+            rotate - 1
+            laser to destroy block - 2 battery levels for laser + 1 for step on destroyed block
+        :param game_map: Map instance for rating
+        :param start_position: started position for rating (start position of bot)
+        :param last_direction: direction of bot
+        :return: rated game map
+        """
+        price = 0
+        position = start_position
+        # (price of near field, position of field, last direction of bot)
+        fields_to_process = [(price, position, last_direction)]
+
+        while fields_to_process:
+            price, position, last_direction = fields_to_process.pop(0)
+
+            field = game_map[position]
+
+            if field.get('field') == FieldType.BLOCK and not self._lasers_enabled:
+                field.update(dict(price=-1))
+                continue
+
+            # update the price
+            field.update(dict(price=price))
+
+            for new_position in get_near_positions(position):
+                new_field = game_map[new_position]
+                assert isinstance(new_field, dict)
+
+                if new_field.get('field') in (FieldType.LASER_BATTERY_BOT, FieldType.BOT):
+                    new_field.update(dict(price=-1))
+                    continue
+
+                # only if next field is cheaper
+                if new_field.get('price', 100000000) > price:
+                    new_direction = get_orientation_position_to_position(position, new_position)
+                    orientation_price = compute_orientation_price(last_direction, new_direction)
+
+                    new_price = sum((
+                        price,  # price of before field
+                        1,  # price for step
+                        orientation_price,  # price of changing orientation
+                        1 if self._batteries_enabled else 0,  # price of battery drain for step
+                        (2 + 1) if self._lasers_enabled and new_field.get('field') == FieldType.BLOCK else 0,
+                        # price of charging battery for laser + firing (only if is target BLOCK)
+                    ))
+
+                    # VERY IMPORTANT FOR EFFICIENCY - insert new data sorted,
+                    # first item in tuple is price and it's sorted ASC,
+                    # so while cycles process at first the data with lowest price
+                    insort(fields_to_process, (new_price, new_position, new_direction))
+
+        return game_map
+
+
+class PathFinder():
+    def __init__(self, game: dict):
+        self._game = game
+        self._game_map = Map(game.get('map'))
+        self._map_rating = MapRating(game, self._game_map)
+        _, self._target_position = min({self._game_map[position].get('price'): position for position in
+                                        get_field_occurrences(FieldType.TREASURE, self._game_map)}.items())
+
+    def get_path(self):
+        return self._resolve_path(
+            self._map_rating.get_rated_map(),
+            self._target_position
+        )
+
+    def _resolve_path(self, game_map: Map, target_position: tuple):
+        target_price = game_map[target_position].get('price')
+        assert isinstance(target_price, int) and target_price > 0
+
+        actual_position = target_position
+
+        path = [target_position]
+        while actual_position != self._map_rating._start_bot_position:  # and price != target_price:
+            near_positions = {game_map[pos].get('price'): pos for pos in get_near_positions(actual_position) if
+                              pos not in path}
+            if not near_positions:
+                print(repr(game_map))
+                exit(1)
+            price, next_position = min(near_positions.items())
+
+            path.append(next_position)
+            actual_position = next_position
+        return tuple(reversed(path))
+
+
+post('http://localhost:44822/admin',
+     dict(
+         map_height=MAP_HEIGHT,
+         map_width=MAP_WIDTH,
+         bots=1,
+         treasures=5,
+         blocks=10,
+         maze_game=True,
+         battery_game=True,
+         laser_game=True
+     ))
+
 
 
 def compute_orientation_price(start_orientation: int, wanted_orientation: int) -> int:
@@ -109,109 +227,27 @@ def get_field_occurrences(field_type: int, game_map: Map, **other_conditions: di
     return found_with_conditions
 
 
-def solve(game_map: Map, start_bot_position: tuple, start_orientation: int, batteries: bool, lasers: bool):
-    def rate_game_map(game_map: Map, position: tuple=start_bot_position, last_direction: int=start_orientation):
-        """Rate game map by price of actions:
-            step - 1 + 1 if batteries are enabled
-            rotate - 1
-            laser to destroy block - 2 battery levels for laser + 1 for step on destroyed block
-        :param game_map: Map instance for rating
-        :param position: started position for rating (start position of bot)
-        :param last_direction: direction of bot
-        :return: rated game map
-        """
-        price = 0
+import time
 
-        data = [(price, position, last_direction)]
 
-        while data:
-            price, position, last_direction = data.pop(0)
-
-            field = game_map[position]
-
-            if field.get('field') == BLOCK and not lasers:
-                field.update(dict(price=-1))
-                continue
-
-            # update the price
-            field.update(dict(price=price))
-
-            for new_position in get_near_positions(position):
-                new_field = game_map[new_position]
-                assert isinstance(new_field, dict)
-
-                if new_field.get('field') in (LASER_BATTERY_BOT, BOT):
-                    new_field.update(dict(price=-1))
-                    continue
-
-                # only if next field is cheaper
-                if new_field.get('price', 100000000) > price:
-                    new_direction = get_orientation_position_to_position(position, new_position)
-                    orientation_price = compute_orientation_price(last_direction, new_direction)
-
-                    new_price = sum((
-                        price,  # price of before field
-                        1,  # price for step
-                        orientation_price,  # price of changing orientation
-                        1 if batteries else 0,  # price of battery drain for step
-                        (2 + 1) if lasers and new_field.get('field') == BLOCK else 0,
-                        # price of charging battery for laser + firing (only if is target BLOCK)
-                    ))
-
-                    # VERY IMPORTANT FOR EFFICIENCY - insert new data sorted,
-                    # first item in tuple is price and it's sorted ASC,
-                    # so while cycles process at first the data with lowest price
-                    insort(data, (new_price, new_position, new_direction))
-
-        return game_map
-
-    def create_path_from_rated_game_map(game_map: Map, target_position: tuple):
-        target_price = game_map[target_position].get('price')
-        assert isinstance(target_price, int) and target_price > 0
-
-        actual_position = target_position
-
-        path = [target_position]
-        while actual_position != start_bot_position:  # and price != target_price:
-            price, next_position = min(
-                {game_map[pos].get('price'): pos for pos in get_near_positions(actual_position) if
-                 pos not in path}.items())
-
-            path.append(next_position)
-            actual_position = next_position
-        return tuple(reversed(path))
-
-    rate_game_map(game_map, start_bot_position, start_orientation)
-
-    treasure_prices = {game_map[position].get('price'): position for position in
-                       get_field_occurrences(TREASURE, game_map)}
-
-    treasure_price, treasure_position = min(treasure_prices.items())
-    path = create_path_from_rated_game_map(game_map, treasure_position)
-
-    print('From {} to {} through {} steps: {}.'.format(start_bot_position, treasure_position, len(path), path))
-    return game_map
+def solve(game: dict):
+    start = time.process_time()
+    path_finder = PathFinder(game)
+    path = path_finder.get_path()
+    end = time.process_time()
+    print('In {} ms through {} steps from {} to {}.'.format(int((end - start) * 1000), len(path), path[0], path[-1]))
+    return path
 
 
 def main():
-    bot_id = get('http://localhost:44822/init').json().get('bot_id')
-    game = get('http://localhost:44822/game/{}'.format(bot_id)).json()
-
-    game_map = game.get('map')
-
-    LASER_GAME = game.get('game_info', {}).get('laser_game', False)
-    BATTERY_GAME = game.get('game_info', {}).get('battery_game', False)
-    game_map = Map(game_map)
-
-    bot_fields = get_field_occurrences(LASER_BATTERY_BOT if BATTERY_GAME or LASER_GAME else BOT, game_map,
-                                       your_bot=True)
-
-    rated_map = solve(game_map, bot_fields[0], game_map[bot_fields[0]].get('orientation'), BATTERY_GAME,
-                      LASER_GAME)
+    while True:
+        bot_id = get('http://localhost:44822/init').json().get('bot_id')
+        game = get('http://localhost:44822/game/{}'.format(bot_id)).json()
+        solve(game)
+        break
 
 
 if __name__ == "__main__":
     main()
-
 
 
